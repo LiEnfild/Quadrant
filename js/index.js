7d229044fa06b63e4d17fc075fe1adb8b3596181
LiEnfild <stasgon@bk.ru>
2017-04-03 13:52:10 +0000

    index.html

--- js/index.js
+++ js/index.js
@@ -5,26 +5,28 @@ var c;
 var D;
 var x1, x2;
 
-function quadro(){
+function quadro() {
     a = Number(prompt("Number a"));
-    if(a===0){
-        alert ("Must be no 0!");
+    if (a === 0) {
+        alert("Must be no 0!");
         a = Number(prompt("Number a"));
     }
     b = Number(prompt("Number b"));
     c = Number(prompt("Number c"));
-    D = Math.pow(b,2) - 4*a*c;
-    if(D < 0){
-         document.write("No rezult");
-         return D;
-    }else if(D === 0){
+    D = Math.pow(b, 2) - 4 * a * c;
+    if (D < 0) {
+        document.write("No rezult");
+        return D;
+    }
+    else if (D === 0) {
         document.write("If D = 0, then only one rezult ");
-        x1 = -b/(2*a);
+        x1 = -b / (2 * a);
         document.write("x1 = " + x1);
         return x1;
-    }else{
-        x1 = (-b + Math.sqrt(D))/(2*a);
-        x2 = (-b - Math.sqrt(D))/(2*a);
+    }
+    else {
+        x1 = (-b + Math.sqrt(D)) / (2 * a);
+        x2 = (-b - Math.sqrt(D)) / (2 * a);
         document.write("x1 = " + x1 + "<br>");
         document.write("x2 = " + x2 + "<br>");
     }
